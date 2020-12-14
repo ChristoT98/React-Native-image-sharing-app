97bbd79e7849d24733a9292585b73719b798ac24
ChristoT98 <christolakshan@gmail.com>
2020-12-14 22:38:44 +0530

    Created image sharing function

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png';
 import * as ImagePicker from 'expo-image-picker'
+import * as Sharing from 'expo-sharing'
 
 export default function App() {
     const [selectedImage, setSelectedImage] =React.useState(null);
@@ -23,6 +24,16 @@ export default function App() {
         setSelectedImage({localUri: pickerResult.uri});
     };
 
+    /*Check if sharing available on trying platform*/
+    let openShareDialogAsync = async ()=> {
+        if(!(await Sharing.isAvailableAsync())){
+            alert(`Oops, sharing isn't available on your platform`);
+            return;
+        }
+
+        await  Sharing.shareAsync(selectedImage.localUri);
+    }
+
     if(selectedImage !== null){
         return (
             <View style={styles.container}>
@@ -30,6 +41,9 @@ export default function App() {
                     source={{ uri:selectedImage.localUri }}
                     style={styles.thumbnail}
                 />
+                <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+                    <Text style={styles.buttonText}>Share this photo</Text>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -115,5 +129,6 @@ const styles = StyleSheet.create({
       width: 300,
         height: 300,
         resizeMode: "contain",
+        marginBottom: 20,
     },
 });
